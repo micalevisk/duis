27e58e7678513a4ca7d48954513e5cce047077f9
jonasferreirab <jfb@icomp.ufam.edu.br>
2019-04-03 16:22:58 -0400

    removendo da cache

--- example/gitaddsubmodule.sh
+++ example/gitaddsubmodule.sh
@@ -4,6 +4,8 @@
 username="${1,,}"
 turma="${2:-CB01}"
 
+git rm --cached "./${turma}/${username}"
+
 git submodule add -b master \
   "https://github.com/${username}/ProgWeb" \
   "./${turma}/${username}"
