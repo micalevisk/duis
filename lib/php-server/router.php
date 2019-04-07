eae479369365dc170bd44d2aa323bbf736b9d5a2
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-04-06 23:21:49 -0400

    bug: usando o `router` não renderiza os `index` files

--- lib/php-server/router.php
+++ lib/php-server/router.php
@@ -11,6 +11,11 @@ if ($URI != '/' && $URI != '//')
 $abf = preg_replace('/^\/+|\/$/', '', $URI);
 $fs = glob("$f/*");
 foreach ($fs as $one) {
+  // FIXME: não renderiza o `index`
+  // if (preg_match('/^index\./i', basename($one))) {
+  //   header('Location: '.$f);
+  // }
+
   $one = str_replace($f.'/', '', $one);
   echo "<a href='" . urlencode("$abf/$one") . "'>$one</a><br/>";
 }
