6a7f5961106eb95c4aa3eb9f55d354ede75b4bc4
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-04-28 20:57:54 -0400

    add: handling event `cleanup`

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const duis = require('./src')
 
-module.exports = async () => {
+async function duisBootstrap() {
   if (process.argv.length <= 2) return // TODO: show help
 
   // assuming: $ duis <PATH/TO/CONFIG-FILE> <PATH/TO/TRAB-FILE>
@@ -15,15 +15,13 @@ module.exports = async () => {
   const configFilename = 'duis.config.js'
   const configFileAbsPath = path.resolve(pathToConfigFile, configFilename)
 
-  console.log(`sobre: ${pathToTrabFile}`)
-  console.log(`usando: ${configFileAbsPath}`)
-
   setupProcessListeners()
   return await duis(configFileAbsPath, pathToTrabFile)
 }
 
-
 function setupProcessListeners() {
+  process.stdin.resume() // the program will not close instantly
+
   /** @see https://nodejs.org/api/process.html#process_event_unhandledrejection */
   process.on('unhandledRejection', (reason, p) => {
     console.log('Unhandled Rejection at', p, 'reason:', reason)
@@ -32,13 +30,16 @@ function setupProcessListeners() {
 
   /** @see https://stackoverflow.com/a/12506613 */
   process.stdin.setRawMode(true)
-  process.stdin.resume()
   process.stdin.setEncoding('utf8')
   process.stdin.on('data', function stdinOnData(key) {
     // ctrl-c (end of text) or Esc
     if (key === '\u0003' || key === '\x1b') {
-      process.exit()
+      process.emit('cleanup')
+      process.emit('exit')
+      return
     }
-    process.stdout.write(key)
   })
 }
+
+
+module.exports = duisBootstrap
